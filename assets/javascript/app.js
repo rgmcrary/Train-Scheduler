8d998403c15f03a890833b25637cad73e4ba091a
Rich McRary <rgmcrary@gmail.com>
2017-08-05 17:35:12 -0400

    Completed Homework
    Added MomentJS and added CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,12 +47,37 @@ function resetFields() {
 }
 
 function calculateTimes(frequency, initialTime) {
-  var arrivalVal;
-  var timeVal;
 
-  //calculations go here
+    var tFrequency = frequency;
 
-  return { arrival: arrivalVal, minutesAway: timeVal };
+
+    var firstTime = initialTime;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+  return { arrival: moment(nextTrain).format("hh:mm"), minutesAway: tMinutesTillTrain};
 }
 
 dataRef.ref().on(
@@ -89,3 +114,5 @@ dataRef.ref().on(
     console.log("Errors handled: " + errorObject.code);
   }
 );
+
+
